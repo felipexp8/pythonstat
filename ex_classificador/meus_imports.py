72efe2f6bc5d23698357ad472d6ae9bc8da38265
Felipe <felipeh@alunos.utfpr.edu.br>
2020-01-31 18:56:25 -0300

    exercicios da aula...

---
+++ ex_classificador/meus_imports.py
@@ -0,0 +1,20 @@
+from sklearn.linear_model import SGDClassifier 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+from tabulate import tabulate
+
+import plotly.express as px
+import pandas as pd
+
+from ipywidgets import interact, interactive, fixed, interact_manual, Layout
+import ipywidgets as widgets
+
+from IPython.display import HTML
+
+import numpy as np
+
+import pylab as plt
+
+print "pronto"
